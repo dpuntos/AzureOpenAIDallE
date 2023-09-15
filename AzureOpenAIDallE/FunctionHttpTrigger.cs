243d1a6b9906480518bb755128ca50b795c1e29b
Dani <daniel.puntos@t-systems.com>
2023-09-15 15:51:08 +0200

    The response is managed and waits until the status is successful. Other improvements.

--- AzureOpenAIDallE/FunctionHttpTrigger.cs
+++ AzureOpenAIDallE/FunctionHttpTrigger.cs
@@ -1,15 +1,17 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Text.Json;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net;
 
 namespace AzureOpenAIDallE
 {
@@ -25,39 +27,49 @@ namespace AzureOpenAIDallE
         {
             //log.LogInformation("C# HTTP trigger function processed a request.");
 
-            const string resourceName = ""; // Your azure resource name
+            const string resource_name = ""; // Your azure resource name
             const string api_key = ""; // Your function api key
-            const string api_ver = "2023-06-01-preview";
-            const string serviceURL = $"https://{resourceName}.openai.azure.com/openai/images/generations:submit?api-version={api_ver}";
+            //TODO: use settigns to get/set resurcename and api key
 
-            string description = req.Query["desc"];
+            const string api_ver = "2023-06-01-preview";
             string url_img_download = String.Empty;
 
-            HttpClient client = new HttpClient();
-            var requestMessage = new HttpRequestMessage(HttpMethod.Post, serviceURL);
-            var requestObjectMessage = new ObjectMessage(description, 1, "1024x1024");
-            requestMessage.Content = new StringContent(JsonConvert.SerializeObject(requestObjectMessage), Encoding.UTF8, "application/json");
-            requestMessage.Headers.Add("api-key", api_key);
-            var responseMessage = await client.SendAsync(requestMessage);
+            string description = req.Query["desc"];
 
-            if (responseMessage.StatusCode == System.Net.HttpStatusCode.Accepted)
+            using (var client = new HttpClient())
             {
-                var definition = new OperationMessage("", "");
-                var jsonResponse = JsonConvert.DeserializeAnonymousType(await responseMessage.Content.ReadAsStringAsync(), definition);
-                var operation_id = jsonResponse.Id;
+                var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.BaseAddress = new Uri($"https://{resource_name}.openai.azure.com/");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                client.DefaultRequestHeaders.Add("api-key", api_key);
+                var data = new { prompt = description, n = 1, size = "1024x1024" };
+                var requestObjectMessage = new ObjectMessage(description, 1, "1024x1024");
 
-                var ImgURL = $"https://{resourceName}.openai.azure.com/openai/operations/images/{operation_id}?api-version={api_ver}";
+                var content = JsonSerializer.Serialize(data);
+                var httpContent = new StringContent(content, Encoding.UTF8, "application/json");
+                var service_response = await client.PostAsync($"openai/images/generations:submit?api-version={api_ver}", httpContent);
 
-                var requestMessageImage = new HttpRequestMessage(HttpMethod.Get, ImgURL);
-                requestMessageImage.Headers.Add("api-key", api_key);
-                Thread.Sleep(6000);//TODO: Manage service call and wait until image status is completed.
-                var responseMessageImage = await client.SendAsync(requestMessageImage);
-                var definitionImage = new ImageMessage();
-                var messImg = JsonConvert.DeserializeObject<ImageMessage>(await responseMessageImage.Content.ReadAsStringAsync());
+                if (service_response.StatusCode == HttpStatusCode.Accepted)
+                {
+                    var imgURL = service_response.Headers.GetValues("operation-location").FirstOrDefault();
+                    var image_response = await client.GetAsync(imgURL);
+                    var image_content_response = await image_response.Content.ReadAsStringAsync();
 
-                url_img_download = messImg.result.data[0].url;
+                    string status = JsonSerializer.Deserialize<OperationMessage>(image_content_response).Status;
+                    int max = 1;
+                    while (status != "succeeded" && max < 20)
+                    {
+                        Thread.Sleep(500);
+                        max++;
+                        image_response = await client.GetAsync(imgURL);
+                        image_content_response = await image_response.Content.ReadAsStringAsync();
+                        status = JsonSerializer.Deserialize<OperationMessage>(image_content_response).Status;
+                    }
 
-                return new RedirectResult(url_img_download);
+                    image_content_response = await image_response.Content.ReadAsStringAsync();
+                    url_img_download = JsonSerializer.Deserialize<ImageMessage>(image_content_response).result.data[0].url;
+                    return new RedirectResult(url_img_download);
+                }
             }
 
             return new OkObjectResult(url_img_download);
